c83ec6183be6ab74d2ab954a510410409fb29a90
Todd Deshane <deshantm@gmail.com>
2010-05-20 18:25:27 -0400

    fix scripts and install so that start-guest script works properly

--- install.sh
+++ install.sh
@@ -9,10 +9,35 @@ fi
 
 cd `dirname $0`
 cp ./bin/* /usr/local/bin/
-mkdir -p /etc/ovs-testing/guest1
-mkdir -p /etc/ovs-testing/guest2
-echo '00:11:22:AA:BB:DD' > /etc/ovs-testing/guest1/mac_addr
-echo '00:11:22:CC:AA:DD' > /etc/ovs-testing/guest2/mac_addr
+mkdir -p /usr/local/etc/ovs-testing/guests/guest1
+mkdir -p /usr/local/etc/ovs-testing/guests/guest2
+echo '00:11:22:AA:BB:DD' > /usr/local/etc/ovs-testing/guests/guest1/mac_addr
+echo '00:11:22:CC:AA:DD' > /usr/local/etc/ovs-testing/guests/guest2/mac_addr
+
+mkdir -p /usr/local/etc/ovs-testing/guests/guest1/copy_into_filesystem/etc/network
+mkdir -p /usr/local/etc/ovs-testing/guests/guest2/copy_into_filesystem/etc/network
+cat > /usr/local/etc/ovs-testing/guests/guest1/copy_into_filesystem/etc/network/interfaces <<EOT
+auto lo
+iface lo inet loopback
+
+auto eth0 
+iface eth0 inet static
+address 10.0.0.201
+netmask 255.255.255.0 
+EOT
+
+cat > /usr/local/etc/ovs-testing/guests/guest2/copy_into_filesystem/etc/network/interfaces <<EOT
+auto lo
+iface lo inet loopback
+
+auto eth0 
+iface eth0 inet static
+address 10.0.0.202
+netmask 255.255.255.0 
+EOT
+
+mkdir -p /usr/local/etc/
+cp ./etc/* /usr/local/etc/
 
 cd $1
 if [ ! -e datapath/linux-2.6/openvswitch_mod.ko ]; then
@@ -20,5 +45,8 @@ if [ ! -e datapath/linux-2.6/openvswitch_mod.ko ]; then
   exit 1
 else
   mkdir -p /usr/local/src/
-  ln -s $1 /usr/local/src/ovs-src
+  ln -sf $1 /usr/local/src/ovs-src
 fi
+
+
+
